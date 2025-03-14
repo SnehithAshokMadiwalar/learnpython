64b267db91ec8dffa9f1a34ed1c020d904bc08d2
SnehithAshokMadiwalar <snehithashokmadiwalar@gmail.com>
2025-03-14 15:18:24 +0530

    Create task41.py

---
+++ task41.py
@@ -0,0 +1,12 @@
+#Using the __init__ Method
+class Oneday:
+    def __init__(self, day, night):
+        self.day = day
+        self.night = night
+
+p1 = Oneday(12, 12)
+
+print(p1.day)
+print(p1.night)
+            
+        
