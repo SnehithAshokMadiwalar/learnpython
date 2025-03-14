e4447fb3125f7aa22c492fe5fba2cb68f637025b
SnehithAshokMadiwalar <snehithashokmadiwalar@gmail.com>
2025-03-14 15:12:26 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,10 @@
+#Create a Simple Class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
