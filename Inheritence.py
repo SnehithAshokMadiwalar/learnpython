4e96d23d20600cc38c94ed6dcb7f6c5685f616f7
SnehithAshokMadiwalar <snehithashokmadiwalar@gmail.com>
2025-03-14 11:54:21 +0530

    Create Inheritence.py

---
+++ Inheritence.py
@@ -0,0 +1,12 @@
+#cc
+#inheritence
+class Name:
+     def head(self):
+         print("iam head")
+class HAVE(Name):
+     def goat(self):
+         print("Goat") 
+HAVE1=HAVE()
+
+HAVE1.goat()
+HAVE1.head()
