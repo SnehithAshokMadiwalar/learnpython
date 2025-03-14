d1c8115767ee0b5195460dfd663285c2f7048e03
SnehithAshokMadiwalar <snehithashokmadiwalar@gmail.com>
2025-03-14 22:08:26 +0530

    Create task5kOperator Overloading.py

---
+++ task5kOperator Overloading.py
@@ -0,0 +1,11 @@
+ 
+#Operator Overloading
+class Number:
+ def __init__(self, value):
+   self.value = value
+ def __add__(self, other):
+   return Number(self.value + other.value)
+num1 = Number(10)
+num2 = Number(20)
+result = num1 + num2
+print(result.value)
